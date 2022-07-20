aeb61a1885d1641fe217fcc296aea4ff970c57d3
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-07-19 21:19:18 -0300

    criacao do site da bedog

---
+++ js/database.js
@@ -0,0 +1,196 @@
+// Banco de dados dos produtos
+
+const produtos = [
+  {
+    id: 1,
+    img: "./img/bedog/AUMOR NO CASACO.png",
+    nameItem: "Aumor no Casaco",
+    description:
+      "Moletom aumor no casado para seu pet ficar em grande estilo...",
+    value: 59.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Blusas"],
+  },
+  {
+    id: 2,
+    img: "./img/bedog/camisa social.png",
+    nameItem: "Camisa Social Bedog",
+    description:
+      "Camisa social para seu pet se destacar nos eventos...",
+    value: 69.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 3,
+    img: "./img/bedog/camiseta ohana.png",
+    nameItem: "Camiseta Ohana",
+    description:
+      "Para compartilhar o amor em família com o seu pet...",
+    value: 74.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 4,
+    img: "./img/bedog/moletom-forever-hungry.jpg",
+    nameItem: "Moletom Forever Hungry",
+    description:
+      "Moletom Forever Hungry para seu pet que vive faminto...",
+    value: 69.90,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Blusas"],
+  },
+  {
+    id: 5,
+    img: "./img/bedog/Anos 60 - 01.png",
+    nameItem: "Peitoral Anos 60",
+    description:
+      "Peitoral Branco com Bolinhas Pretas",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 6,
+    img: "./img/bedog/Damas de Copas - 02.png",
+    nameItem: "Peitoral Dama de Copas",
+    description:
+      "Peitoral Dama de Copas",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 7,
+    img: "./img/bedog/Mickey - 02.png",
+    nameItem: "Peitoral do Mickey",
+    description:
+      "Peitoral do Mickey",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 8,
+    img: "./img/bedog/Minnie - FUNDO TRANSPARENTE.png",
+    nameItem: "Peitoral da Minnie",
+    description:
+      "Peitoral da Minnie",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 9,
+    img: "./img/bedog/Dog Maravilha - 02.png",
+    nameItem: "Peitoral Dog Maravilha",
+    description:
+      "Peitoral Dog Maravilha",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 10,
+    img: "./img/bedog/São Paulo Masculino - 02.png",
+    nameItem: "Peitoral do São Paulo Masculino",
+    description:
+      "Peitoral do São Paulo para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 11,
+    img: "./img/bedog/São Paulo Feminino - 02.png",
+    nameItem: "Peitoral do São Paulo Feminino",
+    description:
+      "Peitoral do São Paulo para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 12,
+    img: "./img/bedog/Corinthians Masculino - 02.png",
+    nameItem: "Peitoral do Corinthians Masculino",
+    description:
+      "Peitoral do Corinthians para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 13,
+    img: "./img/bedog/Corinthians Feminino - 02.png",
+    nameItem: "Peitoral do Corinthians Feminino",
+    description:
+      "Peitoral do Corinthians para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 14,
+    img: "./img/bedog/Santos Feminino - 02.png",
+    nameItem: "Peitoral do Santos Feminino",
+    description:
+      "Peitoral do Santos para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 15,
+    img: "./img/bedog/Santos Masculino - 02.png",
+    nameItem: "Peitoral do Santos Masculino",
+    description:
+      "Peitoral do Santos para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 16,
+    img: "./img/bedog/Palmeiras Masculino - 02.png",
+    nameItem: "Peitoral do Palmeiras Masculino",
+    description:
+      "Peitoral do Palmeiras para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 16,
+    img: "./img/bedog/Palmeiras Feminino - 02.png",
+    nameItem: "Peitoral do Palmeiras Feminino",
+    description:
+      "Peitoral do Palmeiras para seu pet torcer com você nos grandes clássicos...",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 17,
+    img: "./img/bedog/Superdog - 02.png",
+    nameItem: "Peitoral do Superdog",
+    description:
+      "Peitoral superdog",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  {
+    id: 17,
+    img: "./img/bedog/Batdog - 02.png",
+    nameItem: "Peitoral do Batdog",
+    description:
+      "Peitoral Batdog",
+    value: 79.99,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Peitoral"],
+  },
+  
+];
+
---
+++ js/index.js
@@ -0,0 +1,277 @@
+const vitrine = document.querySelector('.vitrine')
+
+
+function criarVitrine(array){
+
+    
+
+    for(let i = 0; i < array.length; i++){
+    
+    let productsDatail = document.createElement('li')
+    let productsPhoto = document.createElement('div')
+    let productsImg = document.createElement('img')
+    let productsCard = document.createElement('div')
+    let productsCategory = document.createElement('span')
+    let productsName = document.createElement('h2')
+    let productsDescription = document.createElement('p')
+    let productsPrice = document.createElement('span')
+    let productsButton = document.createElement('button')
+   
+
+    productsDatail.className = 'produtos'
+    productsPhoto.className = 'fundo-photo'
+    productsCard.className = 'card'
+    productsCategory.className = 'categoria'
+    productsName.className = 'nome-produto'
+    productsDescription.className = 'descricao-produto'
+    productsPrice.className = 'price'
+    productsButton.className = 'button-card'
+    productsImg.className = 'imagem'
+
+    productsImg.src = array[i].img
+    productsCategory.innerText = array[i].tag
+    productsName.innerText = array[i].nameItem
+    productsDescription.innerText = array[i].description
+    productsPrice.innerText = `R$ ${array[i].value.toString().replace('.', ',')}`
+    productsButton.innerText = array[i].addCart
+        
+    let id = produtos[i].id
+    productsButton.setAttribute('id', id)
+    
+    productsPhoto.appendChild(productsImg)
+    productsCard.append(productsCategory, productsName, productsDescription, productsPrice, productsButton)
+    productsDatail.append(productsPhoto, productsCard)
+    vitrine.appendChild(productsDatail)
+        
+    }
+}
+criarVitrine(produtos)
+
+const todos = document.querySelector('.todos')
+const peitoral = document.querySelector('#peitoral')
+const camisetas = document.querySelector('#camisetas')
+const blusas = document.querySelector('#blusas')
+
+todos.addEventListener('click', filtroTodos)
+peitoral.addEventListener('click', filtroPeitoral)
+camisetas.addEventListener('click', filtroCamisetas)
+blusas.addEventListener('click', filtroBlusas)
+
+
+function filtro(array, tag){
+    let result = []
+
+    for(let i = 0; i < array.length; i++){
+        if(array[i].tag == tag){
+            result.push(array[i])
+        }
+    }
+    criarVitrine(result)
+}
+
+function filtroTodos(){
+    vitrine.innerHTML = ''
+    criarVitrine(produtos)
+}
+
+function filtroPeitoral(){
+    vitrine.innerHTML = ''
+    filtro(produtos, 'Peitoral')
+    
+}
+
+function filtroCamisetas(){
+    vitrine.innerHTML = ''
+    filtro(produtos, 'Camisetas')
+}
+
+function filtroBlusas(){
+    vitrine.innerHTML = ''
+    filtro(produtos, 'Blusas')
+}
+
+
+
+let formulario = document.querySelector('.pesquisa-itens')
+let pesquisaItens = document.querySelector('.pesquisa-itens input')
+let pesquisaBotao = document.querySelector('.pesquisa-itens button')
+
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault()
+    let pesquisaProdutos = pesquisaItens.value.toLowerCase().trim()
+    
+    let resultadoBusca = busca(pesquisaProdutos)
+    
+    criarVitrine(resultadoBusca)
+})
+
+
+function busca(valorPesquisa){
+    let resultBusca = []
+    let produtoNaoEncontrado = 'Produto não encontrado'
+
+    vitrine.innerHTML = ''
+    for(let i = 0; i < produtos.length; i++){
+        let nomeProduto = produtos[i].nameItem.toLowerCase().trim()
+        let categoriaProduto = produtos[i].tag[0].toLowerCase().trim()
+        
+        if(valorPesquisa === nomeProduto || valorPesquisa === categoriaProduto){
+       
+           
+            pesquisaItens.value = ''
+            pesquisaItens.focus()
+            resultBusca.push(produtos[i])
+            
+        } 
+   
+    }
+    if(resultBusca.length == 0){
+        vitrine.innerHTML = produtoNaoEncontrado
+                
+        pesquisaItens.value = ''
+        pesquisaItens.focus()
+        return 
+    }
+    return resultBusca
+    
+}
+
+
+
+vitrine.addEventListener('click', interceptandoProduto)
+
+let carrinhoDeCompras = document.querySelector('.lista-carrinho')
+
+
+let carrinho = []
+
+function interceptandoProduto(event){
+    let btnAddCart = event.target
+
+    if(btnAddCart.tagName == 'BUTTON'){
+        let idProduto = btnAddCart.id
+        
+        let produto = produtos.find(function(produto){
+            if(produto.id == idProduto){
+                return produto
+            }
+        }) 
+        adicionarItemCarrinho(produto)
+    }
+}
+
+function adicionarItemCarrinho(produto){
+    
+
+    if(produto !== undefined){
+        carrinhoDeCompras.innerHTML = ''
+        carrinho.push(produto)
+    }
+
+    criarCardCarrinho(carrinho)
+    resultadosItensEValor(carrinho)
+}
+
+carrinhoDeCompras.addEventListener('click', interceptandoCarrinho)
+
+function interceptandoCarrinho(event){
+    // carrinhoVazio.innerHTML = 'Carrinho vazio'
+    let btnRemove = event.target
+    
+    if(btnRemove.tagName === 'BUTTON'){
+        let idRemove = btnRemove.id
+        let removerItem = carrinho.findIndex(elemento => idRemove == elemento.id)
+       
+        carrinho = carrinho.filter((ele, i) => i !== removerItem)
+        carrinhoDeCompras.innerHTML = ''
+        criarCardCarrinho(carrinho)
+    }
+    resultadosItensEValor(carrinho)
+}
+
+
+function criarCardCarrinho(carrinho){
+
+
+    for(let i = 0; i < carrinho.length; i++){
+        
+        let nome = carrinho[i].nameItem
+        let imgProduto = carrinho[i].img
+        let value = carrinho[i].value
+        let id = carrinho[i].id
+   
+        let listCarrinho = document.createElement('li')
+        let divImgCarrinho = document.createElement('div')
+        let imgCarrinho = document.createElement('img')
+        let divCarrinho = document.createElement('div')
+        let nomeCarrinho = document.createElement('h2')
+        let priceCarrinho = document.createElement('span')
+        let buttonCarrinho = document.createElement('button')
+
+        buttonCarrinho.setAttribute("id", id)
+
+        listCarrinho.className = 'carrinho-compras-lista'
+        divImgCarrinho.className = 'imagem-carrinho'
+        divCarrinho.className = 'ajustar-carrinho'
+        nomeCarrinho.className = 'titulo-carrinho'
+        priceCarrinho.className = 'preco-carrinho'
+        buttonCarrinho.className = 'botao-carrinho'
+
+        imgCarrinho.src = imgProduto
+        nomeCarrinho.innerText = nome
+        priceCarrinho.innerText = `R$ ${value.toString().replace('.', ',')}`
+        buttonCarrinho.innerText = 'Remover produto'
+
+        divImgCarrinho.appendChild(imgCarrinho)
+        divCarrinho.append(nomeCarrinho, priceCarrinho, buttonCarrinho)
+        listCarrinho.append(divImgCarrinho, divCarrinho)
+        carrinhoDeCompras.appendChild(listCarrinho)
+
+    }
+    
+}
+
+let carrinhoItens = document.querySelector('.carrinho-itens')
+let somaItensEValor = document.querySelector('.somaItensEValor')
+
+
+
+function resultadosItensEValor(){
+    let resultadoPreco = 0
+        somaItensEValor.innerHTML = ''
+        for(let i = 0; i < carrinho.length; i++){
+            resultadoPreco = resultadoPreco + carrinho[i].value
+        }
+    
+        let divQuantidade = document.createElement('div')
+        let paragrafoQuantidade = document.createElement('p')
+        let paragrafoTotalProdutos = document.createElement('p')
+        let divValorTotal = document.createElement('div')
+        let paragrafoPreco = document.createElement('p')
+        let paragrafoTotalPreco = document.createElement('p')
+        let botaoFinalizar = document.createElement('button')
+
+        divQuantidade.className = 'quantidade-produtos'
+        paragrafoQuantidade.className = 'quantidade'
+        paragrafoTotalProdutos.className = 'total-produtos'
+        divValorTotal.className = 'valor-total'
+        paragrafoPreco.className = 'preco'
+        paragrafoTotalPreco.className = 'preco-total'
+        botaoFinalizar.className = 'botao-finalizar'
+
+        paragrafoQuantidade.innerText = 'Quantidade: '
+        paragrafoTotalProdutos.innerText = `${carrinho.length}`
+        paragrafoPreco.innerText = 'Preço: '
+        paragrafoTotalPreco.innerText = `R$ ${resultadoPreco.toFixed(2).toString().replace('.',',')}`
+        botaoFinalizar.innerText = 'Finalizar compras'
+
+        divQuantidade.append(paragrafoQuantidade, paragrafoTotalProdutos)
+        divValorTotal.append(paragrafoPreco, paragrafoTotalPreco)
+        somaItensEValor.append(divQuantidade, divValorTotal, botaoFinalizar)
+
+}
+resultadosItensEValor()
+
+
+
